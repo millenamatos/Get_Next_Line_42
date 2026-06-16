0be978b5194bb97aeac3877d113155202253701f
Millena Matos <matosmillena015@gmail.com>
2026-06-16 20:54:03 -0300

    gnl

---
+++ get_next_line_utils.c
@@ -0,0 +1,78 @@
+#include "get_next_line.h"
+
+int ft_strlen(const char *str)
+{
+    int i;
+
+    if (!str)
+        return (0);
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+char *ft_strdup(const char *src)
+{
+    int i;
+    int size;
+    char *dst;
+
+    if (!src)
+        return (NULL);
+    size = ft_strlen(src);
+    dst = malloc(size + 1);
+    if (dst == NULL)
+        return (NULL);
+    i = 0;
+    while (src[i])
+    {
+        dst[i] = src[i];
+        i++;
+    }    
+    dst[i] = '\0';
+    return (dst);
+}
+
+char *ft_strchr(const char *str, int c)
+{
+    int i;
+
+    if (!str)
+        return (NULL);
+    i = 0;
+    while (1)
+    {
+        if (str[i] == (char)c)
+            return ((char *)&str[i]);
+        if (str[i] == '\0')
+            return (NULL);
+        i++;
+    }
+}
+
+char *ft_strjoin(const char *s1, const char *s2)
+{
+    int i;
+    int j;
+    int s1_len;
+    int s2_len;
+    char *new_str;
+
+    if (!s1 && !s2)
+        return (NULL);
+    s1_len = ft_strlen(s1);
+    s2_len = ft_strlen(s2);
+    new_str = malloc(s1_len + s2_len + 1);
+    if (new_str == NULL)
+        return (NULL);
+    i = 0;
+    j = 0;
+    while (s1[i])
+        new_str[i++] = s1[i];
+    while (s2[j])
+    
+        new_str[i++] = s2[j++];
+    new_str[i] = '\0';
+    return (new_str);
+}
