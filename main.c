478595b863517f5ce125c693dcdacae218f9ef32
Millena Matos Dos Santos <mmatos-d@c3r2p5.42sp.org.br>
2026-07-08 16:41:44 -0300

    gnl

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
+#include <stdio.h> //printf
+#include <stdlib.h> //free
+#include <fcntl.h> //open e O_RDONLY
 #include "get_next_line.h"
 
 int main(void)
@@ -8,10 +8,9 @@ int main(void)
     int fd;
     char *line;
 
-    fd = open("teste.txt", O_RDONLY); //abre o arquivo para leitura
-    if (fd == -1) //se retornar -1, significa que ocorreu erro (arquivo não existe, sem permissão, etc)
-    {
-        printf("Erro\n");
+    fd = open("teste.txt", O_RDONLY); //abre o arquivo apenas para leitura
+    if (fd == -1)
+        printf("Erro ao abrir o arquivo\n");
         return (1);
     }
     while ((line = get_next_line(fd)) != NULL) //chama gnl repetidamente até retornar null, a cada chamada ela retorna uma linha
@@ -19,6 +18,6 @@ int main(void)
         printf("[%s]\n", line); //imprime entre [] pra facilitar leitura
         free(line); //libera memória alocada por gnl
     }
-    close (fd); //fecha o arquivo para liberar o fd
+    close (fd); //fecha o arquivo após finalizar a leitura
     return(0);
 }
